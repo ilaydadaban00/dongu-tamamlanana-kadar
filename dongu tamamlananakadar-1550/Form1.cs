9feff331a0e7f035c23b8d327f945f9ba8baeac7
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-27 04:09:36 +0300

    Add files via upload

---
+++ dongu tamamlananakadar-1550/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dongu_tamamlananakadar_1550
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           for( int a =0; a < 10;a++)
+            { 
+            
+                MessageBox.Show("döngü calışıyor");
+                if(a == 5)
+                {
+                    MessageBox.Show("donguyu durdur");
+                    break;
+                }
+            }
+        }
+    }
+}
